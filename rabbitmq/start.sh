40a3ce8928c4dbcfbf99d7f5f852972baba59508
consolethinks <consolethinks@gmail.com>
2024-03-22 11:17:16 +0100

    remove rabbitmq

--- rabbitmq/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-docker build "$SCRIPT_DIR/rabbitmq-inited" -t rabbitmq-inited:latest
-docker-compose -p scicatlive up -d
--- rabbitmq/stop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker-compose -p scicatlive down
